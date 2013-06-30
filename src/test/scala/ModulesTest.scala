8b4145294911aee3517a0a782a47084a19d7ea4b
Felix Geller <fgeller@gmail.com>
2013-06-30 17:32:19 +1200

    Adds modules.

--- src/test/scala/ModulesTest.scala
+++ src/test/scala/ModulesTest.scala
@@ -4,13 +4,29 @@ import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class ModulesTest extends FunSpec with ShouldMatchers {
+  case class Module(name: String, dependsOn: Set[Module] = Set())
+  case class DependencyGraph(graph: Graph[Module, Nothing] = Empty) {
+    def isEmpty = graph isEmpty
+  }
+
+  describe("A module") {
+
+    it("has a name") {
+      val testName = "test"
+      Module(name = testName).name should be("test")
+    }
+
+    it("depends on other modules") {
+      val leafModule = Module(name = "leaf")
+      leafModule.dependsOn should be('empty)
+
+      val hans = Module(name = "hans", dependsOn = Set(leafModule))
+      hans.dependsOn should be(Set(leafModule))
+    }
+
+  }
 
   describe("Dependency graphs") {
-
-    case class Module(name: String, dependsOn: Set[Module])
-    case class DependencyGraph(graph: Graph[Module, Nothing] = Empty) {
-      def isEmpty = graph isEmpty
-    }
 
     it("start out empty") {
       val dependencyGraph = DependencyGraph()
